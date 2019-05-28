76a90a65e340aab9577f7494cf91297886e89ab8
sdcb <sdflysha@qq.com>
2019-05-28 18:17:02 +0800

    add scheduler controller.

---
+++ Controllers/Scheduler/SchedulerController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+using System.IO;
+using System.Threading.Tasks;
+using SystemFile = System.IO.File;
+
+namespace DevOps.Controllers.Scheduler
+{
+    public class SchedulerController : Controller
+    {
+        public SchedulerController(IDbConnection db)
+        {
+            this.db = db;
+            Directory.CreateDirectory(BaseDirectory);
+        }
+
+        public async Task<IActionResult> UploadLinq(IFormFile uploadedFile)
+        {
+            if (!uploadedFile.FileName.EndsWith(".linq")) return BadRequest("File extension need be .linq");
+            string directory = EnsureDirectory(Path.GetFileNameWithoutExtension(uploadedFile.FileName));
+
+            using (FileStream file = SystemFile.OpenWrite(Path.Combine(directory, "Runner.linq")))
+            using (Stream destStream = uploadedFile.OpenReadStream())
+            {
+                await destStream.CopyToAsync(file);
+            }
+
+            return Ok();
+        }
+
+        public async Task<IActionResult> DownloadLinq(string id)
+        {
+            string directory = EnsureDirectory(id);
+            return File(Path.Combine(directory, "Runner.linq"), "application/octet-stream");
+        }
+
+        private static string EnsureDirectory(string segment)
+        {
+            string directory = Path.Combine(BaseDirectory, segment);
+            Directory.CreateDirectory(directory);
+            return directory;
+        }
+
+        private static string BaseDirectory { get; } = Path.Combine(Program.BaseDirectory, "Scheduler");
+        private IDbConnection db;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Serilog;
+using System;
+using System.IO;
 
 namespace DevOps
 {
     public class Program
     {
+        public static readonly string BaseDirectory =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DevOps");
+
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
